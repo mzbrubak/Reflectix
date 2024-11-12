5e506217efb2424c324d9cb946551b920b7d55e1
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2024-11-11 20:28:28 -0800

    Double Mirror Piece
    Added a piece that is mirrored on both sides.  Note that with 45 degree rotations, it's possible for the laser to hit the piece parallel to the surfaces of both mirrors (i.e. go between them).  Currently the piece is set up so that if that happens, the laser will just go through unobstructed.  That behavior can be changed if needed though!

---
+++ double_mirror.gd
@@ -0,0 +1,22 @@
+extends Node2D
+@export var state=Vector3i(1,0,0)#direction of normal vector of mirror.
+var destroyed=false
+var state_sprite_dictionary={}#since the state of the sprite and the orientation of the mirror
+#map to each other in a 1-to-1 fashion, this keeps track of which sprite corresponds to which state
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func destroy():
+	print("A mirror has been destroyed!")
+	$MirrorSurface1/Mirror.set_deferred("disabled",true)
+	$MirrorSurface2/Mirror.set_deferred("disabled",true)
+	$OpaqueSurface/Casing.set_deferred("disabled",true)
+	#$OpaqueSurface/Rubble.set_deferred("disabled",false)
