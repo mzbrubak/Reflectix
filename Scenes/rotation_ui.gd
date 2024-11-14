770ac6476392ac1d9bf566b5706a290ab38d3591
Morgan  Brubaker <43830512+mzbrubak@users.noreply.github.com>
2024-11-14 11:12:12 -0800

    fixed rotation accessibility check
    previous version of check led to incorrect results for lasers, it should now be fixed

--- Scenes/rotation_ui.gd
+++ Scenes/rotation_ui.gd
@@ -14,27 +14,42 @@ func on_rotate_selection(rotate_selection):
 	$".".visible = rotate_selection
 
 func on_piece(piece_node):
-	rotate_piece=piece_node.get_parent()
-	for rotation_button in rotation_buttons:
-		rotation_button.disabled=!is_rotation_accessible(rotation_button,rotate_piece.state)
+	if piece_node==null:
+		rotate_piece=null
+	else:
+		rotate_piece=piece_node.get_parent()
+		for rotation_button in rotation_buttons:
+			rotation_button.disabled=!is_rotation_accessible(rotation_button,rotate_piece.state)
 
 func is_rotation_accessible(rotation_button, state)->bool:
-	if state==Vector3i(0,0,1):
+	var newstate=rotation_button.state
+	if state==Vector3i(0,0,1):#straight up
 		if rotation_button.state==Vector3(0,0,1):
 			return false
 		else:
 			return true
-	else:
-		var newstate=rotation_button.state
-		if (state.x==newstate.x)&&(state.y==newstate.y)&&(state.z!=newstate.z):
-			return true
-		elif (state.x==newstate.x)&&(state.y!=newstate.y)&&(state.z==newstate.z):
-			return true
-		elif (state.x!=newstate.x)&&(state.y==newstate.y)&&(state.z==newstate.z):
-			return true
+	elif state.z==1:#up + a 2D direction
+		if newstate==state:
+			return false
 		elif newstate==Vector3i(0,0,1):
 			return true
-		elif state.z==1 and newstate.z==1 and state.x==-state.x and state.y==-state.y:
-			return true
+		elif newstate.z==0:
+			return (state.x==newstate.x&&state.y==newstate.y)
+		elif state.x==-newstate.x&&state.y==-newstate.y:
+			return true #can flip by rotating through vertical
+		elif abs(state.x)==1 and abs(state.y)==1:
+			return (state.x==newstate.x||state.y==newstate.y)
 		else:
+			return (state.x!=-newstate.x&&state.y!=-newstate.y)
+	else:#2D only
+		if newstate==state:
 			return false
+		elif newstate==Vector3i(0,0,1):
+			return true
+		elif newstate.z==1:
+			return (state.x==newstate.x&&state.y==newstate.y)
+		elif abs(state.x)==1 and abs(state.y)==1:
+			return (state.x==newstate.x||state.y==newstate.y)
+		else:
+			return (state.x!=-newstate.x&&state.y!=-newstate.y)
+		
